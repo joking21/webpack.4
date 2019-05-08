d8b1622f06360c8090f588f152ef32a00af4d828
deng.wenwen@datatom.com <123456@qq.com>
2019-05-08 10:32:42 +0800

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,6 +10,7 @@ module.exports = merge(common, {
         compress: true,
         hot: true,
         port: 9000,
+		historyApiFallback: true,  //  webpack4需要加这句，否则vue和react的路由，只有/#/这种hash模式的跳转之后，才能刷新，/tt,这种路由跳转刷新会报404
         stats: 'errors-only',  //只在发生错误时输出
         // host: "0.0.0.0",    // 若希望服务器外部可以访问则是0.0.0.0，默认localhost
         proxy: {
